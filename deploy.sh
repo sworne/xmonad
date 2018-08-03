052eed1cfd8ef02833bf6c053063ede463ac328f
Samuel Worne <sworne@users.noreply.github.com>
2018-08-03 20:48:28 +1000

    Update deploy.sh
    Inclusion of a few missed packages.

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-PKGS="xmonad stow rofi feh compton rxvt-unicode-256color cabal-install fonts-inconsolata"
+PKGS="xmonad stow xsecurelock hsetroot rofi feh compton rxvt-unicode-256color cabal-install fonts-inconsolata"
 HPKG="yaml"
 DIRS="xmonad compton x"
 sudo apt-get install $PKGS
