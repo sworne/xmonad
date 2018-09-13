ad2996f8431d18bec874b9ac48db5e229a65ef08
Samuel Worne <samuel@worne.xyz>
2018-09-13 18:24:13 +1000

    Added better tabs and sidebar layouts.

--- .history/xmonad/.xmonad/xmonad_20180910102759.hs
+++ .history/xmonad/.xmonad/xmonad_20180910102759.hs
@@ -46,6 +46,7 @@ import XMonad.Layout.Maximize
 import XMonad.Layout.BoringWindows (boringWindows)
 import XMonad.Layout.LimitWindows (limitWindows, increaseLimit, decreaseLimit)
 import XMonad.Layout.WindowArranger (windowArrange, WindowArrangerMsg(..))
+import XMonad.Layout.Reflect (reflectVert, reflectHoriz, REFLECTX(..), REFLECTY(..))
 import XMonad.Layout.MultiToggle (mkToggle, single, EOT(EOT), Toggle(..), (??))
 import XMonad.Layout.MultiToggle.Instances (StdTransformers(NBFULL, MIRROR, NOBORDERS))
 import qualified XMonad.Layout.ToggleLayouts as T (toggleLayouts, ToggleLayout(Toggle))
@@ -61,10 +62,6 @@ import XMonad.Layout.TwoPane
 import XMonad.Actions.CopyWindow
 import XMonad.Layout.Gaps
 import XMonad.Layout.Spacing
-import XMonad.Layout.Column
-import XMonad.Layout.BoringWindows
-import XMonad.Layout.IfMax
-import XMonad.Layout.Minimize
 
 
     -- Prompts
@@ -82,26 +79,21 @@ myColorWhite    = "#ffc500"
 myColorRed      = "#ff7322"
 myColorBrown    = "#c0b18b"
 
-myTabConfig = def { 
-                      activeColor = myColorWhite
-                    , activeTextColor = myColorBG
-                    , activeBorderColor = myColorWhite
-                    , inactiveColor = myColorBG
-                    , inactiveTextColor = myColorWhite
-                    , inactiveBorderColor = myColorBG}
+myTabConfig = def { inactiveBorderColor = myColorWhite
+                  , activeTextColor = myColorBG}
 
     -- Settings
 myModMask       = mod4Mask
 myTerminal      = "urxvt"
 myMusic         = "LD_PRELOAD=/usr/lib/libcurl.so.3:~/.xmonad/spotifywm.so $(which spotify)"
 myBrowser       = "google-chrome-stable"
+myNotes         =  myBrowser ++ " --app='https://keep.google.com/'"
 myLauncher      = "rofi -show run"
+myNoise         =  myBrowser ++ " --app='https://asoftmurmur.com/'"
 myLock          = "env XSECURELOCK_SAVER=saver_mplayer xsecurelock"
 myBG            = "hsetroot -solid '" ++ myColorBG ++ "' &"
 myCompositor    = "ps aux |grep '[c]ompton' ||compton &"
-myChat          =  myBrowser ++ " --app='https://chat.google.com/'"
-myNotes         =  myBrowser ++ " --app='https://keep.google.com/'"
-
+myBar           = "xmobar ~/.xmonad/xmobar.hs"
 
 -- For key codes see:
 -- http://hackage.haskell.org/package/xmonad-contrib-0.14/docs/XMonad-Util-EZConfig.html
@@ -131,15 +123,13 @@ myKeys =
     , ("M-l",            spawn myLock)
 
     -- Scratchpads
-    , ("M-`",            namedScratchpadAction scratchpads "music")
-    , ("M-<Page_Up>",    namedScratchpadAction scratchpads "notes")
-    , ("M-<Page_Down>",  namedScratchpadAction scratchpads "chat")
+    , ("M-<Delete>",     namedScratchpadAction scratchpads "music")
+    , ("M-`",            namedScratchpadAction scratchpads "notes")
     ]
 
 -- Scratchpad
 scratchpads = [ NS "notes" spawnNotes findNotes nonFloating
               , NS "music" spawnMusic findMusic nonFloating
-              , NS "chat" spawnChat findChat nonFloating
               ]
 
 -- Notepad
@@ -149,9 +139,6 @@ scratchpads = [ NS "notes" spawnNotes findNotes nonFloating
     -- Music
     spawnMusic  = myMusic
     findMusic   = resource =? "spotify"
-    -- Chat
-    spawnChat   = myChat
-    findChat    = resource =? "chat.google.com"
 
     -- workspaces
 myWorkspaces = ["-", "--", "---" ]
@@ -163,29 +150,28 @@ myManageHook = composeAll
 
 myStartupHook = do
     spawn myCompositor
+    spawn myBar
     spawn myBG
     spawn "unclutter &"
 
-myLayoutHook =  combineTwoP two (gapB $ bar) (gapS $ grid) clss |||
-                combineTwoP two (gapB $ bar) (gapS $ vertical) clss |||
-                combineTwoP two (gapB $ bar) (gapS $ tabs) clss |||
-                combineTwoP two (gapB $ bar) (gapL $ tabs) clss |||
+myLayoutHook =  spacing 20 $ combineTwoP two Full grid clss |||
+                combineTwoP two Full vertical clss |||
+                combineTwoP two Full tab clss |||
+                combineTwoP two Full centered clss |||
+                combineTwoP two Full display clss |||
                 full
     where
-        full = smartBorders $ Full
-        grid = padS $ Grid (4/4)
-        vertical = padS $ Tall 3 (5/100) (50/100)
-        display = gapL $ Full
-        bar =  noBorders (IfMax 2 (Column 1) tabs)
-        tabs = tabbed shrinkText myTabConfig
-        two = Tall 1 (1/50) (1/4)
+        centered = gap Full
+        grid = gap Grid (4/4)
+        vertical = gap Tall 3 (5/100) (50/100)
+        full = smartBorders Full
+        display = gapLarge Full
+        tab = gap $ tabbed shrinkText myTabConfig
+        two = Tall 1 (1/50) (1/5)
 
-        gapS =  gaps [(U,40), (D,10), (L,20), (R,20)]
-        gapM = gaps [(U,60), (D,60), (L,30), (R,30)]
-        gapL = gaps [(U,100), (D,100), (L,100), (R,100)]
-        gapB =  gaps [(U,40), (D,10), (L,0), (R,0)]
-        padS = spacing 10
-        padL = spacing 20
+        rfk = reflectHoriz
+        gap =  gaps [(U,20), (D,20), (L,20), (R,20)]
+        gapLarge = gaps [(U,100), (D,100), (L,100), (R,100)]
         keep = Title "Google Keep" `Or` Role "pop-up"
         spotify = ClassName "Spotify"
         clss = keep `Or` spotify
